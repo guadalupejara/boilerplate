4a9486930dbda48bea63fd99eb676ac528ab0e52
lupe0614 <lupejara0614@gmail.com>
2024-08-11 17:32:43 -0700

    Initial commit

---
+++ page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   <p>I am a boiler plate for Javascript, Tailwind CSS, Flowbite, and Next js installed</p>
+    </main>
+  );
+}
